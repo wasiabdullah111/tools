c3b82049a3e0915fa42c9f1ed1a34d90a7f108d5
wasiabdullah111 <f20ba014@ibitpu.edu.pk>
2024-08-02 02:57:31 +0500

    node

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static('public'));
 
 // Define a route for the root path
 app.get('/', (req, res) => {
-  res.send('<h1>Jazz ka Pakistan.!</h1>');
+  res.send('<h1>Jazz ka apna Pakistan.!</h1>');
 });
 
 // Start the server
